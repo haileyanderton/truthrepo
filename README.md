ee8c494568d57c1f04153aeec635c57d0857c8e6
haileyanderton@gmail.com <hka122095!>
2017-04-23 20:35:35 -0400

    Update
    Bootstrap issues

---
+++ README.md
@@ -0,0 +1,29 @@
+Create DATABASE with 15 topics, 3 verses, 2 quotes
+3 different tables (topics, verses, quotes) connected by foreign key (topic)
+
+http://www.w3resource.com/sql/joins/using-a-where-cluase-to-join-two-tables-related-by-a-single-column-primary-key-or-foriegn-key-pair.php
+
+http://www.w3resource.com/sql/sql-table.php
+
+https://www.quora.com/How-do-you-create-foreign-keys-on-a-table-by-using-phpMyAdmin
+
+https://www.youtube.com/watch?v=F4fTjXSxtE0
+
+https://docs.djangoproject.com/en/1.11/topics/db/models/
+
+https://techarena51.com/index.php/one-to-many-relationships-with-flask-sqlalchemy/
+
+http://flask-appbuilder.readthedocs.io/en/latest/relations.html
+
+CONNECT database my database works, tested it with local host test
+Having trouble understanding when to use sql, when to use flask
+Going to need the ids too to take user to topic page
+Do I need the list route for my app? What exactly does it do?
+What do i need to do to involve the information from the other two tables?
+Read from the database
+Write to the database
+Allow users to write to the database
+Insert new record
+Update existing
+404 page template - error handler
+Link back to homepage
--- db_app.py
+++ db_app.py
@@ -1,10 +1,11 @@
 #!/Users/haileyanderton/Documents/python/flask-project/env/bin/python
 
 import pymysql
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField
+from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
+from wtforms.validators import InputRequired, Length, Regexp, NumberRange
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -27,7 +28,7 @@ app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
 db = SQLAlchemy(app)
 
-# classes: 3 tables and a form
+# classes: 3 tables and 3 forms
 
 
 class Topic(db.Model):
@@ -97,6 +98,61 @@ class TopicForm(FlaskForm):
       ('15', 'Hope') ])
     submit = SubmitField('Submit')
 
+
+class AddVerse(FlaskForm):
+    id_field = HiddenField()
+    old_topic = SelectField('Choose the topic', [ InputRequired()],
+        choices=[
+          ('1', 'Grief'),
+          ('2', 'Forgiveness'),
+          ('3', 'Trust'),
+          ('4', 'Prayer'),
+          ('5', 'Love'),
+          ('6', 'Doubt'),
+          ('7', 'Faith'),
+          ('8', 'Strength'),
+          ('9', 'Money'),
+          ('10', 'Wisdom'),
+          ('11', 'Peace'),
+          ('12', 'Patience'),
+          ('13', 'Friendship'),
+          ('14', 'Perseverance'),
+          ('15', 'Hope') ])
+    new_verse = StringField('Verse', [ InputRequired(),
+        Length(min=3, max=350, message="Invalid length")
+        ])
+    new_book = StringField('Book Chapter:Verse', [ InputRequired(),
+        Length(min=3, max=100, message="Invalid length")
+        ])
+    submit = SubmitField('Add Verse')
+
+class AddQuote(FlaskForm):
+    id_field = HiddenField()
+    old_topic = SelectField('Choose the topic', [ InputRequired()],
+        choices=[
+          ('1', 'Grief'),
+          ('2', 'Forgiveness'),
+          ('3', 'Trust'),
+          ('4', 'Prayer'),
+          ('5', 'Love'),
+          ('6', 'Doubt'),
+          ('7', 'Faith'),
+          ('8', 'Strength'),
+          ('9', 'Money'),
+          ('10', 'Wisdom'),
+          ('11', 'Peace'),
+          ('12', 'Patience'),
+          ('13', 'Friendship'),
+          ('14', 'Perseverance'),
+          ('15', 'Hope') ])
+    new_quote = StringField('Quote', [ InputRequired(),
+            Length(min=3, max=350, message="Invalid length")
+            ])
+    new_author = StringField('Author', [ InputRequired(),
+        Length(min=3, max=100, message="Invalid length")
+        ])
+    submit = SubmitField('Add Quote')
+
 # routes
 
 @app.route('/', methods=['GET'])
@@ -111,9 +167,9 @@ def index():
 def topiclist():
     #'select' is thr variable that holds the select field and choices
     topic = request.form['select']
-    topic_name = Topic.query.filter_by(topic_id=topic).all()
+    # topic_name = Topic.query.filter_by(topic_id=topic).all()
     topics = Topic.query.filter_by(topic_id=topic).all()
-    return render_template('list.html', topic_name=topic_name, topic=topic, topics=topics)
+    return render_template('list.html', topic=topic, topics=topics)
 
 @app.route('/topic/<id>')
 def topic(id):
@@ -123,6 +179,46 @@ def topic(id):
     verse_info = Verse.query.filter_by(topic_id=id).all()
     quote_info = Quote.query.filter_by(topic_id=id).all()
     return render_template('topic.html', topic_info=topic_info, verse_info=verse_info, quote_info=quote_info)
+
+@app.route('/add_verse', methods=['GET', 'POST'])
+def addVerse():
+    form3 = AddVerse()
+    if form3.validate_on_submit():
+        topic_id = request.form['old_topic']
+        verse = request.form['new_verse']
+        book = request.form['new_book']
+        # the data to be inserted into Sock model - the table, socks
+        # Flask-SQLAlchemy magic adds record to database
+        record = Verse(verse, book, topic_id)
+        db.session.add(record)
+        db.session.commit()
+        # create a message to send to the template
+        message = "The new record has been added."
+        return render_template('add.html', message=message)
+    else:
+        # show validaton errors
+        flash('placeholder')
+        return render_template('add.html', form3=form3)
+
+@app.route('/add_quote', methods=['GET', 'POST'])
+def addQuote():
+    form3 = AddQuote()
+    if form3.validate_on_submit():
+        topic_id = request.form['old_topic']
+        quote = request.form['new_quote']
+        author = request.form['new_author']
+        # the data to be inserted into Sock model - the table, socks
+        # Flask-SQLAlchemy magic adds record to database
+        record = Quote(quote, author, topic_id)
+        db.session.add(record)
+        db.session.commit()
+        # create a message to send to the template
+        message = "The new record has been added."
+        return render_template('add.html', message=message)
+    else:
+        # show validaton errors
+        flash('placeholder')
+        return render_template('add.html', form3=form3)
 
 # error handlers
 
